e12769bc956a52ed90d351e6a438e4ba97d2ae4f
Alex <keeper.gen@gmail.com>
2023-06-20 11:13:26 +0300

    Clear the code
    - Clear the code from comment and unused parts

--- main.js
+++ main.js
@@ -1,5 +1,4 @@
 import figmaConfig from './tailwind.config.js'
-
 console.log(`%c==ORIG [ File ]:`, 'color:#FFFFFF; border-radius:4px; background:#253031; padding:4px 8px;', figmaConfig)
 
 const finalObj = {}
@@ -17,32 +16,21 @@ const colors = figmaConfig.theme.colors
 console.log(`%c==ORIG [ Colors OBJ ]:`, 'color: #FFFFFF; border-radius:4px; background: #253031; padding: 4px 8px;', colors)
 
 Object.entries(colors).forEach(([name, color]) => {
-   // 🟩DONE — 01. Разбить "NAME" по тире и получить массив значений
    const nameArr = name.split('-')
-   // console.log(`%cResult ${nameArr.length}:`, 'color: #453643; border-radius:4px; background: #E3E3E3; padding: 4px 8px;', nameArr)
 
-   // 🟩DONE — 02. Перезаписать в массив только те значения, которые НЕ используют "TAILWIND" и "BASE"
    if (nameArr.includes('tailwind') || nameArr.includes('base')) {
       return
    } else {
       nameArr.join()
    }
 
-   // 🟩DONE — 03. Удалить из массива первое значение "мусорный" префикс (например "AAA")
-   // 🟩DONE — 04. Удалить из массива значения где встречается "EXTRAS" и разделить "•"
    const clearResults = nameArr.filter(clearUnused)
-
    function clearUnused(el) {
       if (el !== 'aaa' && el !== 'extras' && el !== '•') {
          return el
       }
    }
 
-   // 🟩DONE — 05. Найти в массиве те значения, где последний элемент = "DEFAULT"
-   // 🟩DONE — 06. Создать условие при котором массивы содержащие (найденые) "DEFAULT" записать в новый массив с ключом "DEFAULT" и значением "HEX код"
-   // 🟩DONE — 07. Для остальных элементов – убрать вариант (например "500"), оставшиеся значения сконкатинировать в одно название через дефис
-   // 🟩DONE — 08. Объеденённое значение сохранить как название нового объекта, в который надо добавить значения (например "имя": "HEX код")
-   // 🟩DONE — 09. Если имя состоит из нескольких элементов (в массиве), их нужно объединить
    if (clearResults[clearResults.length - 1] === 'default') {
       const lastElDefault = clearResults[clearResults.length - 1]
       const beforeDefault = clearResults[clearResults.length - 2]
@@ -57,35 +45,15 @@ Object.entries(colors).forEach(([name, color]) => {
    }
 })
 
-// 🟩DONE — 10. Полученный элемент использовать как название объекта и объединить в массив палеток с цветами
-// 🟩DONE — 11. Элемент eg. "500" добавить в объект как ключ
-// 🟩DONE — 12. Элемент "COLOR" (HEX цвет) добавить в объект как значение в соответствии с ключом
-// 🟩DONE — 13. Каждый объект с градацией по цветам объединить в один общий объект
 function mergeResult(objName, key, value) {
-   // console.log(objName, key, value)
-   // finalObj = {
-   //    [objName]: {
-   //       [key]: value
-   //    }
-   // }
-   // finalArray.push(finalObj)
-
    if (!finalObj[objName]) {
-      console.log(`🟥 Palette "${objName.toUpperCase()}" doesn\'t exist, creating`)
       finalObj[objName] = {}
    }
-   console.log(finalObj[objName], [key]) // Fill the object with results
    finalObj[objName][key] = value
 }
 
-console.log('= = = = = = = = = = = = = = = = = = = = = = = = = = = = ')
-console.log(`%cFinal [ OBJ ]:`, 'color: #FFFFFF; border-radius:4px; background: #2978A0; padding: 4px 8px;', finalObj)
-console.log('= = = = = = = = = = = = = = = = = = = = = = = = = = = = ')
-
-// 🟩DONE — FILL THE HTML TABLE
 setTimeout(fillColorPalette, 1000)
 function fillColorPalette() {
-// 🟩DONE — Add unique palette DIV
    Object.entries(finalObj).forEach(([paletteTitle, variants]) => {
       const resultAreaCode = document.querySelector('.result-area__code')
       const paletteGroup = document.createElement('div')
@@ -97,12 +65,10 @@ function fillColorPalette() {
       paletteGroup.appendChild(paletteElTitle)
       paletteElTitle.innerHTML = `<span style="color: #fd7d0e">'${paletteTitle}'</span><span style="color: #8d8d8d">: {</span>`
 
-      // 🟩DONE — Add each variant with HEX color
       Object.entries(variants).forEach(([name, color]) => {
          const paletteVariant = document.createElement('div')
          paletteVariant.classList.add('color-variant', `${paletteTitle}-${name}`)
          paletteGroup.appendChild(paletteVariant)
-         // 🟩DONE — Add to the HTML page variants, colors and color preview box with styles
          paletteVariant.innerHTML +=
             `<div class="color-variant__color-preview" style="background: ${color}"></div>
             <div class="color-variant__value">'${name}'</div> :&nbsp;
@@ -113,7 +79,6 @@ function fillColorPalette() {
       })
    })
 
-   // 🟩DONE — Move "default" variant from the end to the beginning
    const paletteGroups = document.querySelectorAll('.palette-group')
    paletteGroups.forEach(list => {
       let childEls = list.children
@@ -124,7 +89,6 @@ function fillColorPalette() {
    })
 }
 
-// 🟩DONE — Add the copy button below the area result
 const finalObjJSON = JSON.stringify(finalObj, null, "\t")
 function copyPaletteConfig() {
    navigator.clipboard.writeText(finalObjJSON)
@@ -134,13 +98,11 @@ function copyPaletteConfig() {
 }
 btnCopy.addEventListener('click', copyPaletteConfig)
 
-// 🟩DONE — Restore the copy button values and styles
 function restoreCopyBtn() {
    btnCopy.innerText = 'Copy to clipboard'
    btnCopy.classList.remove('--copied')
 }
 
-// 🟩DONE — Download the JSON file
 const data = "text/json;charset=utf-8," + encodeURIComponent(finalObjJSON)
 const downloadLink = document.createElement('a');
 buttonsArea.appendChild(downloadLink)
@@ -149,7 +111,7 @@ downloadLink.href = 'data:' + data;
 downloadLink.download = 'tailwind-color.config.json';
 downloadLink.innerHTML = 'Download config file';
 
-// 🟨 — Upload code function
+// 🟨 WORK IN PROGRESS — TODO: Upload the config file code function
 const btnUploadLabel = document.createElement('label')
 const btnUploadInput = document.createElement('input')
 btnUploadInput.setAttribute('type', 'file')
